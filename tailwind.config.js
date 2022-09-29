ab886a53d1b976b5501e405422e4ed5a3f0bed4e
sugeng20 <sugeng.wanantara@gmail.com>
2022-09-29 12:11:24 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#FD661F",
+        secondary: "#64748b",
+        dark: "#30333C",
+      },
+    },
+  },
+  plugins: [],
+};
